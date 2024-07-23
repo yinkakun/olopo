9e537671ab7706d1a2f73410651908e1b2a0a0c9
yinkakun <yinkakun@gmail.com>
2024-07-23 12:20:52 +0100

    Try rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import { loadEnv } from "vite";
 
 import cloudflare from "@astrojs/cloudflare";
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: cloudflare({
-    imageService: "cloudflare",
-  }),
+  output: "static",
+  adapter: cloudflare(),
 });
